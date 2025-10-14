172501af8d470bd280a8f36f414873afd7ec626e
Shirel Marino <shirelmr12@gmail.com>
2025-10-14 12:49:42 -0600

    Set Up

---
+++ manber_myers.py
@@ -0,0 +1,44 @@
+class SubstrRank:
+    def __init__(self, left_rank=0, right_rank=0, index=0):
+        self.left_rank = left_rank
+        self.right_rank = right_rank
+        self.index = index
+
+def make_ranks(substr_rank, n):
+    r = 1
+    rank = [-1] * n
+    rank[substr_rank[0].index] = r
+    for i in range(n):
+        if (substr_rank[i].left_rank != substr_rank[i-1].left_rank or
+			substr_rank[i].right_rank != substr_rank[i-1].right_rank):
+            r += 1
+        rank[substr_rank[i].index] = r
+    return rank
+
+def suffix_array(T):
+    n = len(T)
+    substr_rank = []
+
+    for i in range(n):
+        substr_rank.append(SubstrRank(ord(T[i]), ord(T[i + 1]) if i < n-1 else 0, i))
+
+    substr_rank.sort(key=lambda sr: (sr.left_rank, sr.right_rank))
+
+    l = 2
+    while l < n:
+        rank = make_ranks(substr_rank, n)
+
+        for i in range(n):
+            substr_rank[i].left_rank = rank[i]
+            substr_rank[i].right_rank = rank[i+l] if i+l < n else 0
+            substr_rank[i].index = i
+        l *= 2
+
+        substr_rank.sort(key=lambda sr: (sr.left_rank, sr.right_rank))
+
+    SA = [substr_rank[i].index for i in range(n)]
+
+    return SA
+
+SA = suffix_array("mississippi")
+print(SA)
---
+++ sais.py
@@ -0,0 +1,105 @@
+def getBuckets(T):
+    count = {}
+    buckets = {}
+    for c in T:
+        count[c] = count.get(c, 0) + 1
+    start = 0
+    for c in sorted(count.keys()):
+        buckets[c] = (start, start + count[c])
+        start += count[c]
+    return buckets
+
+def sais(T):
+    t = ["_"] * len(T)
+    
+    t[len(T) - 1] = "S"
+    for i in range(len(T) - 1, 0, -1):
+        if T[i-1] == T[i]:
+            t[i - 1] = t[i]
+        else:
+            t[i - 1] = "S" if T[i-1] < T[i] else "L"
+    
+    buckets = getBuckets(T)
+
+    count = {}
+    SA = [-1] * len(T)
+    LMS = {}
+    end = None
+    for i in range(len(T) - 1, 0, -1):
+        if t[i] == "S" and t[i - 1] == "L":
+            revoffset = count[T[i]] = count.get(T[i], 0) + 1
+            SA[buckets[T[i]][1] - revoffset] = i
+            if end is not None:
+                LMS[i] = end
+            end = i
+
+    LMS[len(T) - 1] = len(T) - 1
+    count = {}
+    for i in range(len(T)):
+        if SA[i] >= 0:
+            if t[SA[i] - 1] == "L":
+                symbol = T[SA[i] - 1]
+                offset = count.get(symbol, 0)
+                SA[buckets[symbol][0] + offset] = SA[i] - 1
+                count[symbol] = offset + 1
+
+    count = {}
+    for i in range(len(T) - 1, 0, -1):
+        if SA[i] > 0:
+            if t[SA[i] - 1] == "S":
+                symbol = T[SA[i] - 1]
+                revoffset = count[symbol] = count.get(symbol, 0) + 1
+                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+
+    namesp = [-1] * len(T)
+    name = 0
+    prev = None
+    for i in range(len(SA)):
+        if t[SA[i]] == "S" and t[SA[i] - 1] == "L":
+            if prev is not None and T[SA[prev]:LMS[SA[prev]]] != T[SA[i]:LMS[SA[i]]]:
+                name += 1
+            prev = i
+            namesp[SA[i]] = name
+
+    names = []
+    SApIdx = []
+    for i in range(len(T)):
+        if namesp[i] != -1:
+            names.append(namesp[i])
+            SApIdx.append(i)
+
+    if name < len(names) - 1:
+        names = sais(names)
+
+    names = list(reversed(names))
+
+    SA = [-1] * len(T)
+    count = {}
+    for i in range(len(names)):
+        pos = SApIdx[names[i]]
+        revoffset = count[T[pos]] = count.get(T[pos], 0) + 1
+        SA[buckets[T[pos]][1] - revoffset] = pos
+
+    count = {}
+    for i in range(len(T)):
+        if SA[i] >= 0:
+            if t[SA[i] - 1] == "L":
+                symbol = T[SA[i] - 1]
+                offset = count.get(symbol, 0)
+                SA[buckets[symbol][0] + offset] = SA[i] - 1
+                count[symbol] = offset + 1
+
+    count = {}
+    for i in range(len(T) - 1, 0, -1):
+        if SA[i] > 0:
+            if t[SA[i] - 1] == "S":
+                symbol = T[SA[i] - 1]
+                revoffset = count[symbol] = count.get(symbol, 0) + 1
+                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+
+    return SA
+
+string = "GTCCCGATGTCATGTCAGGA$"
+T = [ord(c) for c in string]
+SA = sais(T)
+print(SA)
