1b3d8ed0a8a0ed8333ccbb3be0c887e39705b99a
Shirel Marino <shirelmr12@gmail.com>
2025-10-20 20:45:01 -0600

    errors

--- manber_myers.py
+++ manber_myers.py
@@ -5,36 +5,44 @@ class SubstrRank:
         self.index = index
 
 def make_ranks(substr_rank, n):
-    r = 1
+    r = 0
     rank = [-1] * n
     rank[substr_rank[0].index] = r
-    for i in range(n):
+    
+    for i in range(1, n):
         if (substr_rank[i].left_rank != substr_rank[i-1].left_rank or
-			substr_rank[i].right_rank != substr_rank[i-1].right_rank):
+            substr_rank[i].right_rank != substr_rank[i-1].right_rank):
             r += 1
         rank[substr_rank[i].index] = r
     return rank
 
 def suffix_array(T):
     n = len(T)
+    
+    chars = sorted(set(T))
+    char_rank = {c: i for i, c in enumerate(chars)}
+    
     substr_rank = []
-
     for i in range(n):
-        substr_rank.append(SubstrRank(ord(T[i]), ord(T[i + 1]) if i < n-1 else 0, i))
+        substr_rank.append(SubstrRank(
+            char_rank[T[i]], 
+            char_rank[T[i + 1]] if i < n - 1 else -1,
+            i
+        ))
 
     substr_rank.sort(key=lambda sr: (sr.left_rank, sr.right_rank))
 
-    l = 2
+    l = 1
     while l < n:
         rank = make_ranks(substr_rank, n)
 
         for i in range(n):
-            substr_rank[i].left_rank = rank[i]
-            substr_rank[i].right_rank = rank[i+l] if i+l < n else 0
-            substr_rank[i].index = i
-        l *= 2
+            next_index = substr_rank[i].index + l
+            substr_rank[i].left_rank = rank[substr_rank[i].index]
+            substr_rank[i].right_rank = rank[next_index] if next_index < n else -1
 
         substr_rank.sort(key=lambda sr: (sr.left_rank, sr.right_rank))
+        l *= 2
 
     SA = [substr_rank[i].index for i in range(n)]
 
@@ -50,7 +58,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
-        filename = "moby_dick.txt"
+        filename = "frankenstein.txt"
     
     SA = main(filename)
     print(SA)
